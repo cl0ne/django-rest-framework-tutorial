11179ac66e4e309bca95b1b411837d0e66eadb40
cl0ne <cl0ne@mithril.org.ua>
2017-06-04 22:56:01 +0300

    Tutorial part 6: Use router

--- snippets/views.py
+++ snippets/views.py
@@ -1,21 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import api_view, permission_classes, detail_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
-from snippets.permissions import IsOwnerOrReadOnly, ReadOnly
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
-
-
-@api_view(['GET'])
-@permission_classes([ReadOnly])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
